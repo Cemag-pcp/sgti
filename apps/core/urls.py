d8ebff8fb2d505fc84541cd8f9730a0e3f692449
luan <luan640@gmail.com>
2026-04-15 09:14:43 -0300

    Fix root redirect for deployed app

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 app_name = 'core'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/tickets/', permanent=False)),
     path('applications/', views.ApplicationListView.as_view(), name='application_list'),
     path('applications/create/', views.ApplicationCreateView.as_view(), name='application_create'),
     path('applications/<int:pk>/edit/', views.ApplicationUpdateView.as_view(), name='application_edit'),
