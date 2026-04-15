f2fd9d266249cdffebef73027d51d6d4363e74b2
luan <luan640@gmail.com>
2026-04-15 09:32:36 -0300

    Set safe production allowed hosts defaults

--- sgti/settings/production.py
+++ sgti/settings/production.py
@@ -3,7 +3,11 @@ from decouple import config, Csv
 
 DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='sgti.onrender.com,.onrender.com,localhost,127.0.0.1',
+    cast=Csv(),
+)
 
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
